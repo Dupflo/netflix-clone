e6ad0aa1f9333f142428cc9bfd508bc232195d80
Florian <floriandupuis1996@gmail.com>
2020-08-23 18:06:07 +0200

    Signup with Firebase

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { firebase } from './lib/firebase.prod';
 import { FirebaseContext } from './context/firebase';
-
-const config = {
-  apiKey: 'AIzaSyBcJ6ROt340l9hBUMEzRmd8kR_x56wxMpU',
-  authDomain: 'netflix-7146f.firebaseapp.com',
-  databaseURL: 'https://netflix-7146f.firebaseio.com',
-  projectId: 'netflix-7146f',
-  storageBucket: 'netflix-7146f.appspot.com',
-  messagingSenderId: '107161095291',
-  appId: '1:107161095291:web:f8f03f6d50c67ff68e66de',
-};
-
-firebase.initializeApp(config);
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase }}>
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyBcJ6ROt340l9hBUMEzRmd8kR_x56wxMpU',
+  authDomain: 'netflix-7146f.firebaseapp.com',
+  databaseURL: 'https://netflix-7146f.firebaseio.com',
+  projectId: 'netflix-7146f',
+  storageBucket: 'netflix-7146f.appspot.com',
+  messagingSenderId: '107161095291',
+  appId: '1:107161095291:web:f8f03f6d50c67ff68e66de',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
