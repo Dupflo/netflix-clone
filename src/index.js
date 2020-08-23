14fa2fff1c9e8b58874ee13d43bbb6159e4e6ab9
Florian <floriandupuis1996@gmail.com>
2020-08-23 15:11:19 +0200

    Firebase and Signin Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: 'AIzaSyBcJ6ROt340l9hBUMEzRmd8kR_x56wxMpU',
+  authDomain: 'netflix-7146f.firebaseapp.com',
+  databaseURL: 'https://netflix-7146f.firebaseio.com',
+  projectId: 'netflix-7146f',
+  storageBucket: 'netflix-7146f.appspot.com',
+  messagingSenderId: '107161095291',
+  appId: '1:107161095291:web:f8f03f6d50c67ff68e66de',
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
-  <>
+  <FirebaseContext.Provider value={{ firebase }}>
     <GlobalStyles />
     <App />
-  </>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
